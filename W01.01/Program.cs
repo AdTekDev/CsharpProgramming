fdfda24fc945e0b87264ed72a34dcef890d5fa89
AdTekDev <nguyenngtu777@gmail.com>
2022-11-07 09:59:06 +0700

    Add files via upload

---
+++ W01.01/Program.cs
@@ -0,0 +1,23 @@
+﻿
+double a, b, c, delta;
+
+Console.WriteLine("Nhap he so A: ");
+a = Convert.ToDouble ( Console.ReadLine() );
+Console.WriteLine("Nhap he so B: ");
+b = Convert.ToDouble( Console.ReadLine() );
+Console.WriteLine("Nhap he so C: ");
+c = Convert.ToDouble( Console.ReadLine() );
+delta = b * b - 4 * a * c;
+
+if (delta > 0)
+{
+    Console.WriteLine("Co 2 nghiem");
+}
+else if (delta == 0)
+{
+    Console.WriteLine("Co 1 nghiem KEP");
+}
+else
+{
+    Console.WriteLine("o nghiem");
+}
