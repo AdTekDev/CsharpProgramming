4e226f30fd912c000a4aeb6be349821b2dfb4159
AdTekDev <nguyenngtu777@gmail.com>
2022-11-09 12:02:38 +0700

    Add files via upload

---
+++ W01.02/w0102_03_limcuongfull.cs
@@ -0,0 +1,44 @@
+﻿
+//  In Hinh Kim Cuong
+
+int H, N, i, j;
+String str;
+
+
+
+Console.WriteLine("Nhap do dai H cua Canh HCN: ");
+H = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Nhap N - so hinh lap lai: ");
+N = Convert.ToInt32(Console.ReadLine());
+
+int KichCo = N * 2 * (H - 1) + 1;
+int Cao = 2 * H - 1;
+int Du, XTam, BienDo;
+
+for (i = 0; i < Cao; i++)
+{
+    str = "";
+    for (j = 0; j < KichCo; j++)
+    {
+        Du = j % (2 * H - 2);
+        XTam = j - Du + H - 1;
+        BienDo = i;
+        if (i > H - 1)
+        {
+            BienDo = H - 1 - (i - (H - 1));
+        } 
+        if (
+            j >= XTam - BienDo &&
+            j <= XTam + BienDo
+            )
+        {
+            str += "* ";
+        }
+        else
+        {
+            str += "  ";
+        }
+    }
+    Console.WriteLine(str);
+}
